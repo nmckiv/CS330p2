e715388336424e9a629fb259a0b0c6b589d9372c
nmckiv <nmckivergan@gmail.com>
2024-10-07 17:09:47 -0400

    Finalized stuff

--- Fall2024-Assignment2-npmckivergan/Fall2024-Assignment2-npmckivergan/wwwroot/script.js
+++ Fall2024-Assignment2-npmckivergan/Fall2024-Assignment2-npmckivergan/wwwroot/script.js
@@ -1,56 +1,72 @@
 // API stuff
-const apiUrl = 'https://api.bing.microsoft.com/v7.0/search';
+const apiUrl = 'https://api.bing.microsoft.com/v7.0/search?';
 const apiKey = '5cfc2670fc954d5486d138575de24ef8';
 const unsplashAccessKey = 'C7cTfj8gueMHbhdxwf90TkSjq7-u-7liwGHxjyeCINA';
 
 // Bing API search function
-function apiSearch(query, lucky = false) {
+//function apiSearch(query, lucky = false) {
+//    $.ajax({
+//        url: apiUrl,
+//        type: 'GET',
+//        data: { q: query },
+//        beforeSend: function (xhr) {
+//            xhr.setRequestHeader('Ocp-Apim-Subscription-Key', apiKey);
+//        },
+//        success: function (data) {
+//            // Redirect if I'm Feeling Lucky was hit
+//            if (lucky) {
+//                if (data.webPages && data.webPages.value.length > 0) {
+//                    window.open(data.webPages.value[0].url, '_blank');
+//                } else {
+//                    alert('No results found for your query.');
+//                }
+//            } else {
+//                // Display results normally
+//                displayResults(data);
+//            }
+//        },
+//    });
+//}
+
+function apiSearch(callback) {
+    var params = {
+        'q': $('#query').val(),
+        'count': 50,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+
     $.ajax({
-        url: apiUrl,
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
         type: 'GET',
-        data: { q: query }, 
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader('Ocp-Apim-Subscription-Key', apiKey);
-        },
-        success: function (data) {
-            // Redirect if I'm Feeling Lucky was hit
-            if (lucky) {
-                if (data.webPages && data.webPages.value.length > 0) {
-                    window.open(data.webPages.value[0].url, '_blank');
-                } else {
-                    alert('No results found for your query.');
-                }
-            } else {
-                // Display results normally
-                displayResults(data);
+        headers: {
+            'Ocp-Apim-Subscription-Key': apiKey
+        }
+    })
+        .done(function (data) {
+            if (callback) {
+                callback(data);
             }
-        },
-    });
-}
+            else {
+                var len = data.webPages.value.length;
+                var results = '';
+                for (i = 0; i < len; i++) {
+                    results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
+                }
 
-// Function to display search results
-function displayResults(data) {
-    let resultsDiv = $('#searchResults');
-    resultsDiv.empty();
-    if (data.webPages && data.webPages.value.length > 0) {
-        data.webPages.value.forEach(function (result) {
-            let resultHtml = `<p><a href="${result.url}" target="_blank">${result.name}</a><br>${result.snippet}</p>`;
-            resultsDiv.append(resultHtml);
+                $('#searchResults').html(results);
+                $('#searchResults').show();
+            }
+        })
+        .fail(function () {
+            alert('error');
         });
-    } else {
-        resultsDiv.append('<p>No results found</p>');
-    }
-    resultsDiv.css('visibility', 'visible');
 }
 
-// Search button click handler
-$('#searchButton').click(function () {
-    let query = $('#query').val();
-    if (query) {
-        apiSearch(query);
-    } else {
-        alert('Please enter a search query');
-    }
+//Search button click handler
+$('#searchButton').on('click', function () {
+    apiSearch();
+    document.getElementById("searchResults").style.visibility = 'visible';
 });
 
 // Search on Enter key
@@ -63,12 +79,9 @@ $('#query').keydown(function (event) {
 
 // "I'm Feeling Lucky" button click handler
 $('#luckyButton').click(function () {
-    let query = $('#query').val();
-    if (query) {
-        apiSearch(query, true);
-    } else {
-        alert('Please enter a search query');
-    }
+    apiSearch(function (data) {
+        window.location.href = data.webPages.value[0].url;
+    });
 });
 
 // Function to change the background image to a random puppy
@@ -79,9 +92,9 @@ function changeBackgroundImage() {
     });
 }
 
-// Change background image on document click, except on buttons
+// Change background image on header click
 $(document).click(function (event) {
-    if (!$(event.target).closest('#searchButton, #timeButton, #luckyButton, #query, #searchResults').length) { // Ignore clicks on buttons
+    if ($(event.target).closest('#searchEngineName').length) { // Ignore clicks on buttons
         changeBackgroundImage();
         $('#searchResults').hide();
     }
